ee91eb743ae007e06683a678c2ed915a5e674d90
Anthony Frehner <frehner@users.noreply.github.com>
2022-03-03 22:51:57 -0700

    things working up until minute 55

--- src/script.js
+++ src/script.js
@@ -3,18 +3,24 @@ import * as dat from "lil-gui";
 import * as React from "react";
 import ReactDOM from "react-dom";
 import { Canvas } from "@react-three/fiber";
-import { PerspectiveCamera, OrbitControls } from "@react-three/drei";
+import {
+  PerspectiveCamera,
+  OrbitControls,
+  useTexture,
+} from "@react-three/drei";
 import * as THREE from "three";
 import { OrbitControls as OGOrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-const { Suspense, StrictMode, useRef, useEffect } = React;
+const { Suspense, StrictMode, useRef, useEffect, useState } = React;
 
 function App() {
   return (
     <StrictMode>
       <Suspense fallback={null}>
         <Canvas gl={{ alpha: false }}>
+          <Fog />
           <House />
+          <Graves />
           <Floor />
           <Lights />
           <Camera />
@@ -25,12 +31,179 @@ function App() {
   );
 }
 
+const bushGeometry = new THREE.SphereBufferGeometry(1, 16, 16);
+const bushMaterial = new THREE.MeshStandardMaterial({ color: "#89c854" });
 function House() {
+  const wallHeight = 2.5;
+
   return (
-    <mesh position={[0, 1, 0]}>
-      <sphereGeometry args={[1, 32, 32]} />
-      <meshStandardMaterial roughness={0.7} />
+    <group>
+      {/* roof */}
+      <mesh
+        position={[0, wallHeight + 0.5, 0]}
+        rotation={[0, Math.PI * 0.25, 0]}
+      >
+        <coneBufferGeometry args={[3.5, 1, 4]} />
+        <meshStandardMaterial color={"#b35f45"} />
+      </mesh>
+      <Walls wallHeight={wallHeight} />
+      <Door />
+      {/* bushes */}
+      <mesh
+        geometry={bushGeometry}
+        material={bushMaterial}
+        scale={[0.5, 0.5, 0.5]}
+        position={[0.8, 0.2, 2.2]}
+      />
+      <mesh
+        geometry={bushGeometry}
+        material={bushMaterial}
+        scale={[0.25, 0.25, 0.25]}
+        position={[1.4, 0.1, 2.1]}
+      />
+      <mesh
+        geometry={bushGeometry}
+        material={bushMaterial}
+        scale={[0.4, 0.4, 0.4]}
+        position={[-0.8, 0.1, 2.2]}
+      />
+      <mesh
+        geometry={bushGeometry}
+        material={bushMaterial}
+        scale={[0.15, 0.15, 0.15]}
+        position={[-1, 0.05, 2.6]}
+      />
+      {/* door light */}
+      <pointLight args={["#ff7d46", 1, 7]} position={[0, 2.2, 2.7]} />
+    </group>
+  );
+}
+
+function Door() {
+  const [
+    doorColorTexture,
+    doorAlphaTexture,
+    doorAmbientOcclusionTexture,
+    doorHeightTexture,
+    doorNormalTexture,
+    doorMetalnessTexture,
+    doorRoughnessTexture,
+  ] = useTexture([
+    "/textures/door/color.jpg",
+    "/textures/door/alpha.jpg",
+    "/textures/door/ambientOcclusion.jpg",
+    "/textures/door/height.jpg",
+    "/textures/door/normal.jpg",
+    "/textures/door/metalness.jpg",
+    "/textures/door/roughness.jpg",
+  ]);
+
+  const doorMeshRef = useRef();
+  useEffect(() => {
+    doorMeshRef.current.geometry.setAttribute(
+      "uv2",
+      new THREE.Float32BufferAttribute(
+        doorMeshRef.current.geometry.attributes.uv.array,
+        2
+      )
+    );
+  }, []);
+  return (
+    <mesh position={[0, 1, 2 + 0.01]} ref={doorMeshRef}>
+      <planeBufferGeometry args={[2.2, 2.2, 100, 100]} />
+      <meshStandardMaterial
+        map={doorColorTexture}
+        transparent={true}
+        alphaMap={doorAlphaTexture}
+        aoMap={doorAmbientOcclusionTexture}
+        displacementMap={doorHeightTexture}
+        displacementScale={0.1}
+        normalMap={doorNormalTexture}
+        metalnessMap={doorMetalnessTexture}
+        roughnessMap={doorRoughnessTexture}
+        // wireframe
+      />
     </mesh>
+  );
+}
+
+function Walls({ wallHeight }) {
+  const [
+    bricksColorTexture,
+    bricksAmbientOcclusionTexture,
+    bricksNormalTexture,
+    bricksRoughnessTexture,
+  ] = useTexture([
+    "/textures/bricks/color.jpg",
+    "/textures/bricks/ambientOcclusion.jpg",
+    "/textures/bricks/normal.jpg",
+    "/textures/bricks/roughness.jpg",
+  ]);
+
+  const wallMeshRef = useRef();
+  useEffect(() => {
+    wallMeshRef.current.geometry.setAttribute(
+      "uv2",
+      new THREE.Float32BufferAttribute(
+        wallMeshRef.current.geometry.attributes.uv.array,
+        2
+      )
+    );
+  }, []);
+  return (
+    <mesh position={[0, wallHeight / 2, 0]} ref={wallMeshRef}>
+      <boxBufferGeometry args={[4, wallHeight, 4]}>
+        <bufferAttribute
+          attachObject={["geometry", "attributes"]}
+          args={["uv2"]}
+        />
+      </boxBufferGeometry>
+      <meshStandardMaterial
+        map={bricksColorTexture}
+        aoMap={bricksAmbientOcclusionTexture}
+        normalMap={bricksNormalTexture}
+        roughnessMap={bricksRoughnessTexture}
+      />
+    </mesh>
+  );
+}
+
+const graveGeometry = new THREE.BoxBufferGeometry(0.6, 0.8, 0.2);
+const graveMaterial = new THREE.MeshStandardMaterial({ color: "#b2b6b1" });
+const graveLocations = [];
+for (let i = 0; i < 50; i++) {
+  const angle = Math.random() * Math.PI * 2;
+  const radius = 3 + Math.random() * 6;
+  const x = Math.sin(angle) * radius;
+  const z = Math.cos(angle) * radius;
+  graveLocations.push({ x, z });
+}
+function Graves() {
+  return (
+    <group>
+      {graveLocations.map((locationObj, i) => (
+        <mesh
+          key={i}
+          geometry={graveGeometry}
+          material={graveMaterial}
+          position={[locationObj.x, 0.3, locationObj.z]}
+          rotation={[
+            0,
+            (Math.random() - 0.5) * 0.4,
+            (Math.random() - 0.5) * 0.4,
+          ]}
+        />
+      ))}
+    </group>
+  );
+}
+
+function Fog() {
+  return (
+    <>
+      <fog args={["#262837", 1, 15]} attach="fog" />
+      <color attach="background" args={["#262837"]} />
+    </>
   );
 }
 
@@ -57,10 +230,10 @@ function Lights() {
   return (
     <>
       {/* ambient light */}
-      <ambientLight args={["#ffffff", 0.5]} ref={ambientLightRef} />
+      <ambientLight args={["#b9d5ff", 0.12]} ref={ambientLightRef} />
       {/* moonLight */}
       <directionalLight
-        args={["#ffffff", 0.5]}
+        args={["#b9d5ff", 0.12]}
         position={[4, 5, -2]}
         ref={moonLightRef}
       />
