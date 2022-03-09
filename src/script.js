7a0fded277438037e8ed3199e81a6e2e7e683ec7
Anthony Frehner <frehner@users.noreply.github.com>
2022-03-08 21:28:27 -0700

    finish lesson

--- src/script.js
+++ src/script.js
@@ -2,14 +2,14 @@ import "./style.css";
 import * as dat from "lil-gui";
 import * as React from "react";
 import ReactDOM from "react-dom";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import {
   PerspectiveCamera,
   OrbitControls,
   useTexture,
+  useHelper,
 } from "@react-three/drei";
 import * as THREE from "three";
-import { OrbitControls as OGOrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const { Suspense, StrictMode, useRef, useEffect, useState } = React;
 
@@ -17,12 +17,16 @@ function App() {
   return (
     <StrictMode>
       <Suspense fallback={null}>
-        <Canvas gl={{ alpha: false }}>
+        <Canvas
+          gl={{ alpha: false }}
+          shadows={{ type: THREE.PCFSoftShadowMap }}
+        >
           <Fog />
           <House />
           <Graves />
           <Floor />
           <Lights />
+          <Ghosts />
           <Camera />
           <Controls />
         </Canvas>
@@ -35,6 +39,28 @@ const bushGeometry = new THREE.SphereBufferGeometry(1, 16, 16);
 const bushMaterial = new THREE.MeshStandardMaterial({ color: "#89c854" });
 function House() {
   const wallHeight = 2.5;
+  const doorPointLightRef = useRef();
+  // useHelper(doorPointLightRef, THREE.PointLightHelper, 1, "cyan");
+  // useEffect(() => {
+  //   gui
+  //     .add(doorPointLightRef.current.position, "x")
+  //     .name("doorx")
+  //     .min(-5)
+  //     .max(5)
+  //     .step(0.001);
+  //   gui
+  //     .add(doorPointLightRef.current.position, "y")
+  //     .name("doory")
+  //     .min(-5)
+  //     .max(5)
+  //     .step(0.001);
+  //   gui
+  //     .add(doorPointLightRef.current.position, "z")
+  //     .name("doorz")
+  //     .min(-5)
+  //     .max(5)
+  //     .step(0.001);
+  // }, []);
 
   return (
     <group>
@@ -54,27 +80,39 @@ function House() {
         material={bushMaterial}
         scale={[0.5, 0.5, 0.5]}
         position={[0.8, 0.2, 2.2]}
+        castShadow
       />
       <mesh
         geometry={bushGeometry}
         material={bushMaterial}
         scale={[0.25, 0.25, 0.25]}
         position={[1.4, 0.1, 2.1]}
+        castShadow
       />
       <mesh
         geometry={bushGeometry}
         material={bushMaterial}
         scale={[0.4, 0.4, 0.4]}
         position={[-0.8, 0.1, 2.2]}
+        castShadow
       />
       <mesh
         geometry={bushGeometry}
         material={bushMaterial}
         scale={[0.15, 0.15, 0.15]}
         position={[-1, 0.05, 2.6]}
+        castShadow
       />
       {/* door light */}
-      <pointLight args={["#ff7d46", 1, 7]} position={[0, 2.2, 2.7]} />
+      <pointLight
+        args={["#ff7d46", 1, 7]}
+        position={[0, 2.2, 2.7]}
+        castShadow
+        ref={doorPointLightRef}
+        shadow-mapSize-width={256}
+        shadow-mapSize-height={256}
+        shadow-camera-far={7}
+      />
     </group>
   );
 }
@@ -151,7 +189,7 @@ function Walls({ wallHeight }) {
     );
   }, []);
   return (
-    <mesh position={[0, wallHeight / 2, 0]} ref={wallMeshRef}>
+    <mesh position={[0, wallHeight / 2, 0]} ref={wallMeshRef} castShadow>
       <boxBufferGeometry args={[4, wallHeight, 4]}>
         <bufferAttribute
           attachObject={["geometry", "attributes"]}
@@ -192,6 +230,7 @@ function Graves() {
             (Math.random() - 0.5) * 0.4,
             (Math.random() - 0.5) * 0.4,
           ]}
+          castShadow
         />
       ))}
     </group>
@@ -208,10 +247,58 @@ function Fog() {
 }
 
 function Floor() {
+  const [
+    grassColorTexture,
+    grassAmbientOcclusionTexture,
+    grassNormalTexture,
+    grassRoughnessTexture,
+  ] = useTexture([
+    "/textures/grass/color.jpg",
+    "/textures/grass/ambientOcclusion.jpg",
+    "/textures/grass/normal.jpg",
+    "/textures/grass/roughness.jpg",
+  ]);
+
+  grassColorTexture.repeat.set(8, 8);
+  grassAmbientOcclusionTexture.repeat.set(8, 8);
+  grassNormalTexture.repeat.set(8, 8);
+  grassRoughnessTexture.repeat.set(8, 8);
+
+  grassColorTexture.wrapS = THREE.RepeatWrapping;
+  grassAmbientOcclusionTexture.wrapS = THREE.RepeatWrapping;
+  grassNormalTexture.wrapS = THREE.RepeatWrapping;
+  grassRoughnessTexture.wrapS = THREE.RepeatWrapping;
+
+  grassColorTexture.wrapT = THREE.RepeatWrapping;
+  grassAmbientOcclusionTexture.wrapT = THREE.RepeatWrapping;
+  grassNormalTexture.wrapT = THREE.RepeatWrapping;
+  grassRoughnessTexture.wrapT = THREE.RepeatWrapping;
+
+  const floorMeshRef = useRef();
+  useEffect(() => {
+    floorMeshRef.current.geometry.setAttribute(
+      "uv2",
+      new THREE.Float32BufferAttribute(
+        floorMeshRef.current.geometry.attributes.uv.array,
+        2
+      )
+    );
+  }, []);
+
   return (
-    <mesh rotation={[-Math.PI * 0.5, 0, 0]} position={[0, 0, 0]}>
+    <mesh
+      rotation={[-Math.PI * 0.5, 0, 0]}
+      position={[0, 0, 0]}
+      ref={floorMeshRef}
+      receiveShadow
+    >
       <planeGeometry args={[20, 20]} />
-      <meshStandardMaterial color={"#a9c388"} />
+      <meshStandardMaterial
+        map={grassColorTexture}
+        aoMap={grassAmbientOcclusionTexture}
+        normalMap={grassNormalTexture}
+        roughnessMap={grassRoughnessTexture}
+      />
     </mesh>
   );
 }
@@ -236,6 +323,7 @@ function Lights() {
         args={["#b9d5ff", 0.12]}
         position={[4, 5, -2]}
         ref={moonLightRef}
+        castShadow
       />
     </>
   );
@@ -255,128 +343,65 @@ function Controls() {
   return <OrbitControls enableDamping={true} />;
 }
 
+function Ghosts() {
+  const ghost1Ref = useRef();
+  const ghost2Ref = useRef();
+  const ghost3Ref = useRef();
+  useFrame((state) => {
+    const elapsedTime = state.clock.getElapsedTime();
+    // update ghost
+    const ghost1Angle = elapsedTime * 0.5;
+    ghost1Ref.current.position.x = Math.cos(ghost1Angle) * 4;
+    ghost1Ref.current.position.z = Math.sin(ghost1Angle) * 4;
+    ghost1Ref.current.position.y = Math.sin(elapsedTime * 3);
+
+    const ghost2Angle = -elapsedTime * 0.32;
+    ghost2Ref.current.position.x = Math.cos(ghost2Angle) * 5;
+    ghost2Ref.current.position.z = Math.sin(ghost2Angle) * 5;
+    ghost2Ref.current.position.y =
+      Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5);
+
+    const ghost3Angle = -elapsedTime * 0.18;
+    ghost3Ref.current.position.x =
+      Math.cos(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.32));
+    ghost3Ref.current.position.z =
+      Math.sin(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.5));
+    ghost3Ref.current.position.y =
+      Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5);
+  });
+
+  // useHelper(ghost3Ref, THREE.PointLightHelper, 1, "cyan");
+  return (
+    <>
+      <pointLight
+        args={["#ff00ff", 2, 3]}
+        ref={ghost1Ref}
+        castShadow
+        shadow-mapSize-width={256}
+        shadow-mapSize-height={256}
+        shadow-camera-far={7}
+      />
+      <pointLight
+        args={["#00ffff", 2, 3]}
+        ref={ghost2Ref}
+        castShadow
+        shadow-mapSize-width={256}
+        shadow-mapSize-height={256}
+        shadow-camera-far={7}
+      />
+      <pointLight
+        args={["#ffff00", 2, 3]}
+        ref={ghost3Ref}
+        castShadow
+        shadow-mapSize-width={256}
+        shadow-mapSize-height={256}
+        shadow-camera-far={7}
+      />
+    </>
+  );
+}
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
-/**
- * Base
- */
 // Debug
 const gui = new dat.GUI();
-
-// // Canvas
-// const canvas = document.querySelector("canvas.webgl");
-
-// // Scene
-// const scene = new THREE.Scene();
-
-// /**
-//  * Textures
-//  */
-// const textureLoader = new THREE.TextureLoader();
-
-// /**
-//  * House
-//  */
-// // Temporary sphere
-// const sphere = new THREE.Mesh(
-//   new THREE.SphereGeometry(1, 32, 32),
-//   new THREE.MeshStandardMaterial({ roughness: 0.7 })
-// );
-// sphere.position.y = 1;
-// scene.add(sphere);
-
-// // Floor
-// const floor = new THREE.Mesh(
-//   new THREE.PlaneGeometry(20, 20),
-//   new THREE.MeshStandardMaterial({ color: "#a9c388" })
-// );
-// floor.rotation.x = -Math.PI * 0.5;
-// floor.position.y = 0;
-// scene.add(floor);
-
-// /**
-//  * Lights
-//  */
-// // Ambient light
-// const ambientLight = new THREE.AmbientLight("#ffffff", 0.5);
-// gui.add(ambientLight, "intensity").min(0).max(1).step(0.001);
-// scene.add(ambientLight);
-
-// // Directional light
-// const moonLight = new THREE.DirectionalLight("#ffffff", 0.5);
-// moonLight.position.set(4, 5, -2);
-// gui.add(moonLight, "intensity").min(0).max(1).step(0.001);
-// gui.add(moonLight.position, "x").min(-5).max(5).step(0.001);
-// gui.add(moonLight.position, "y").min(-5).max(5).step(0.001);
-// gui.add(moonLight.position, "z").min(-5).max(5).step(0.001);
-// scene.add(moonLight);
-
-// /**
-//  * Sizes
-//  */
-// const sizes = {
-//   width: window.innerWidth,
-//   height: window.innerHeight,
-// };
-
-// window.addEventListener("resize", () => {
-//   // Update sizes
-//   sizes.width = window.innerWidth;
-//   sizes.height = window.innerHeight;
-
-//   // Update camera
-//   camera.aspect = sizes.width / sizes.height;
-//   camera.updateProjectionMatrix();
-
-//   // Update renderer
-//   renderer.setSize(sizes.width, sizes.height);
-//   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-// });
-
-// /**
-//  * Camera
-//  */
-// // Base camera
-// const camera = new THREE.PerspectiveCamera(
-//   75,
-//   sizes.width / sizes.height,
-//   0.1,
-//   100
-// );
-// camera.position.x = 4;
-// camera.position.y = 2;
-// camera.position.z = 5;
-// scene.add(camera);
-
-// // Controls
-// const controls = new OGOrbitControls(camera, canvas);
-// controls.enableDamping = true;
-
-// /**
-//  * Renderer
-//  */
-// const renderer = new THREE.WebGLRenderer({
-//   canvas: canvas,
-// });
-// renderer.setSize(sizes.width, sizes.height);
-// renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-// /**
-//  * Animate
-//  */
-// const clock = new THREE.Clock();
-
-// const tick = () => {
-//   const elapsedTime = clock.getElapsedTime();
-
-//   // Update controls
-//   controls.update();
-
-//   // Render
-//   renderer.render(scene, camera);
-
-//   // Call tick again on the next frame
-//   window.requestAnimationFrame(tick);
-// };
-
-// tick();
